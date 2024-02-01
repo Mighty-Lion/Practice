e8e8f54f1f36cd1db0543a46637207adc94c4a51
Aslan Abaev <hedgehog5349@gmail.com>
2024-02-01 15:48:59 +0300

    revert to 17d55ea..ee3f5ad

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,56 +1,17 @@
-import {useEffect, useState} from 'react';
 import {
   Button,
   AuthForm,
   FormWrapper,
   InputWrapper,
   OpenPasswordButton,
-  PasswordImg,
-  LabelForValidate,
-  LabelForInput,
+  PasswordImg, LabelForValidate, LabelForInput,
 } from '@/pages/Login/index.styles';
 import { useValidation } from '@/hooks/useValidation';
 import { usePassInput } from '@/hooks/usePassInput';
 
 export default function Login() {
-  const [rerender, setRerender] = useState(false)
   const { formik } = useValidation();
   const { typeInput, passImg, handlePassInput } = usePassInput();
-  const apiUrl = import.meta.env.VITE_API_URL;
-  const apiClientId = import.meta.env.VITE_API_CLIENT_ID;
-
-  useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const codeParam = urlParams.get('code');
-    console.log(codeParam);
-    async function getAccessToken() {
-      await fetch(`http://localhost:4000/getAccessToken?code=${codeParam}`, {
-        method: 'GET',
-      })
-        .then((responce) => {
-          return responce.json();
-        })
-        .then((data) => {
-          console.log(data);
-
-          if (data.access_token) {
-            localStorage.setItem('accessToken', data.access_token);
-            setRerender(!rerender);
-          }
-        });
-    }
-
-    if (codeParam && localStorage.getItem('accessToken') === null) {
-      getAccessToken();
-    }
-  }, []);
-
-  const loginWithGithub = () => {
-    window.location.assign(
-      `https://github.com/login/oauth/authorize?client_id=${apiClientId}`
-    );
-  };
 
   return (
     <FormWrapper>
@@ -91,9 +52,6 @@ export default function Login() {
           }}
         >
           Submit
-        </Button>
-        <Button type="button" onClick={loginWithGithub}>
-          Login with Github
         </Button>
       </AuthForm>
     </FormWrapper>
