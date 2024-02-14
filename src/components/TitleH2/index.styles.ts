363ca7e66c1fb264df1e5c3619eae0e6c397f042
Aslan Abaev <hedgehog5349@gmail.com>
2024-02-14 18:03:14 +0300

    added TitleH2

---
+++ src/components/TitleH2/index.styles.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const TitleH2 = styled.h2`
+  font-family: Roboto, sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  color: var(--color-white-0);
+`;
