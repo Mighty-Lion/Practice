65a2abb5295ba6f7d1b0385177686457f7397572
Aslan Abaev <hedgehog5349@gmail.com>
2024-02-21 17:25:54 +0300

    fix styles

--- src/components/Text/index.styles.ts
+++ src/components/Text/index.styles.ts
@@ -3,6 +3,8 @@ import styled from '@emotion/styled';
 export const Text = styled.span`
   font-weight: 400;
   font-size: 14px;
+  color: var(--primary-text-color);
+  transition: var(--transition-color);
 `;
 
 export const TextBold = styled(Text)`
